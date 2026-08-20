b8761283eb8f2d7ca8824c624a36f688769fc224
Fariz <mfariz23@iitk.ac.in>
2026-08-20 08:20:07 +0000

    add phone number field to schema

--- apps/user-app/lib/auth.ts
+++ apps/user-app/lib/auth.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { phoneNumber } from "better-auth/plugins";
 // If your Prisma file is located elsewhere, you can change the path
 import { prisma } from "@repo/db";
 
@@ -7,7 +8,16 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
-    emailAndPassword: {
-        enabled: true,
-    },
+    plugins: [
+    phoneNumber({
+      sendOTP: async ({ phoneNumber, code }, ctx) => {
+        if (process.env.NODE_ENV === "development") {
+          console.log(`[AUTH OTP] To: ${phoneNumber} | Code: ${code}`);
+          return;
+        }
+
+        // Add production SMS provider here
+      },
+    }),
+  ],
 });
